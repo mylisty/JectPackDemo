0191beaefd0816721ab6610e4530eb329ba4acd3
maoshengtao <mst0405@163.com>
2020-07-01 17:44:54 +0800

    Initial commit

---
+++ app/src/main/java/com/jectpack/demo/BeanTest.java
@@ -0,0 +1,12 @@
+package com.jectpack.demo;
+
+import java.io.Serializable;
+
+/**
+ * Create by maoshengtao on 2020/7/1 0001
+ * Describe:
+ * Iteration
+ */
+public class BeanTest implements Serializable {
+    public  String name;
+}
